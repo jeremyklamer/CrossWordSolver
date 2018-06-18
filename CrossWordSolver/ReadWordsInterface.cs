08b7eed8866cd20129f9613b19475a852ff28ea5
Jeremy Klamer <Jeremy Klamer@gmail.com>
2018-06-18 12:06:38 +0200

    Aded NL word files. Added basic interfaces.

---
+++ CrossWordSolver/ReadWordsInterface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossWordSolver
+{
+    interface ReadWordsInterface
+    {
+        Words InitWordsFromFiles(List<string> filePaths);        
+    }
+}
---
+++ CrossWordSolver/Words.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossWordSolver
+{
+    internal class Words
+    {
+        private List<List<string>> wordLists = new List<List<string>>();
+
+        public Words()
+        {            
+         
+        }
+
+        /// <summary>
+        /// Get a list of words with a certain lenght. Returns null when none found.
+        /// </summary>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public List<string> GetWords(int length)
+        {            
+            return wordLists[length - 1];
+        }
+
+        public void SetWords(int length, List<string> words)
+        {
+            wordLists.Insert(length - 1, words);
+        }
+
+
+    }
+}
